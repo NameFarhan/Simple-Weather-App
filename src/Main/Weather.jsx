529196f73e094588b8a8c286048dc204f595bf5f
NameFarhan <Farhanalibwn1215@gmail.com>
2024-07-28 10:55:22 +0500

    finishing

--- src/Main/Weather.jsx
+++ src/Main/Weather.jsx
@@ -2,7 +2,9 @@ import './Weather.css'
 import axios from 'axios';
 import { useState } from 'react';
 import locationIcon from '../Images/map.svg'
-
+import humidityIcon from '../Images/hum.svg'
+import windIcon from '../Images/wind.svg'
+import line from '../Images/line.svg'
 const Weather = () =>{
     const [location, setLocation] = useState(''); 
     const [weatherData, setWeatherData] = useState(null); // State to store fetched weather data
@@ -23,7 +25,13 @@ const Weather = () =>{
           setWeatherData(null); // Clear previous weather data
         }
       };
-
+      const getCurrentDate = () => {
+        const date = new Date();
+        const options = {month: 'long', day: 'numeric' };
+        return date.toLocaleDateString(undefined, options);
+      };
+    
+      
     return (
         <>
         <div className="Wrapper">
@@ -39,10 +47,33 @@ const Weather = () =>{
       </div>
 
       <div className='weather-Details-Box'>
+      {weatherData ? (
+        <div className='Details-box-inner'>
+          {/* <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+          <p>Condition: {weatherData.weather[0].description}</p> */}
+          <p style={{fontSize:'18px',color:'#fff',textAlign:'center'}}>Today, {getCurrentDate()}</p>   
+          <p style={{fontSize:'100px',color:'#fff',textAlign:'center',marginTop:'10px'}}>{Math.round(weatherData.main.temp)}°</p>
+          <p style={{fontSize:'24px',fontWeight:'700',color:'#fff',textAlign:'center',marginTop:'10px'}}>{weatherData.weather[0].description}</p>
+<div className='hum-and-wind-box1'>
+<div className='box1'>
+    <img src={windIcon} alt="wind icon is here" />
+    <p style={{marginLeft:'22px'}}>Wind</p>
+    <img style={{height:'21px',marginLeft:'22px'}} src={line} alt="vartical line" />
+    <p style={{marginLeft:'22px'}}>{weatherData.wind.speed} m/s</p>
+</div>
+<div className='box2'>
 
+</div>
+</div>
+        </div>
+      ) : <p style={{fontSize:'24px',fontWeight:'700',color:'#fff',display:'flex',justifyContent:'center',alignItems:'center',textAlign:'center'}}>Please Enter The Location Above</p>}
       </div>
 
-      <button className='fetchWeather' onClick={fetchWeather}>Get Weather</button>
+      <button className='fetchWeather-button' onClick={fetchWeather}>Get Weather</button>
+      {error && <p>{error}</p>} {/* Display error message if there is any */}
             </div>
         </div>
         </>
